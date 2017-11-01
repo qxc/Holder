1da5b92346e338303720f8a1f3d0d49772c976e1
z <cold.ferret@gmail.com>
2017-11-01 11:10:00 -0700

    Basic setup, object that rotates. Balls that spawn. How to give them velocity?

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+    public Transform ball;
+    int xRange = 5;
+    int yRange = 5;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("spawnBall",0.0f,1.0f);
+	}
+	void spawnBall()
+    {
+        int x = Random.Range(-xRange, xRange);
+        int y = Random.Range(-yRange, yRange);
+        var newBall = Instantiate(ball, new Vector3(x, y, 0), Quaternion.identity);
+        newBall.Translate(-10, 0, 0);
+        //Rigidbody2D rb = ball.GetComponent<Rigidbody2D>();
+        //rb.velocity = new Vector3(0, 10000, 0);
+
+    }
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    int speed = 70;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    void OnTriggerEnter2D(Collider2D center)
+    {
+        print("Ball Entered");
+    }
+
+    // Update is called once per frame
+
+    void Update () {
+
+        if (Input.GetKey("s"))
+        {
+            //print("s pushed");
+            transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+        }
+        else if(Input.GetKey("d"))
+        {
+            //print("d pushed");
+            transform.Rotate(Vector3.back * Time.deltaTime * speed);
+        }
+    }
+}
